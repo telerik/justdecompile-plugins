b5262be7898bf32dd6f2ef3470d44fe011fb7214
TELERIK\manev <TELERIK\manev>
2012-07-23 10:26:26 +0300

    CAI added for assembly version generation

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Telerik")]
+[assembly: AssemblyCopyright("Copyright © Telerik 2002-2012")]
+[assembly: AssemblyProduct("JustDecompile")]
+
+[assembly: AssemblyVersion("2012.1.720.46")]
+[assembly: AssemblyFileVersion("2012.1.720.46")]
