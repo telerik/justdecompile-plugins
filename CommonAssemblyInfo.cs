18b82a884bd1e444033844b64f43c96d73667f62
TELERIK\manev <TELERIK\manev>
2012-07-23 11:33:01 +0300

    version fixes

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,8 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyCompany("Telerik")]
-[assembly: AssemblyCopyright("Copyright © Telerik 2002-2012")]
-[assembly: AssemblyProduct("JustDecompile")]
-
-[assembly: AssemblyVersion("2012.2.723.5")]
-[assembly: AssemblyFileVersion("2012.2.723.5")]
+[assembly: AssemblyVersion("2012.2.720.46")]
+[assembly: AssemblyFileVersion("2012.2.720.46")]
