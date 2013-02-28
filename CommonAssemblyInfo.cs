969e7d075fcf587b0c209e0b07dd1dcc3871c0b5
TELERIK\TeamFoundationUser <TELERIK\TeamFoundationUser>
2013-02-28 17:08:00 +0200

    Auto-Build: Version Update [***NO_CI***]

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2012.2.228.1")]
-[assembly: AssemblyFileVersion("2012.2.228.1")]
+[assembly: AssemblyVersion("2012.2.228.2")]
+[assembly: AssemblyFileVersion("2012.2.228.2")]
