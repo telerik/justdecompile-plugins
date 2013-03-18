cbcf29a9fa4b09114948381e2ef615a6937274e2
TELERIK\miivanov <TELERIK\miivanov>
2013-03-18 15:16:24 +0200

    added GoToEntryPoint plugin to sourcecontrol and to JustDecompile Plugins Build

---
+++ GoToEntryPoint/MenuItem.cs
@@ -0,0 +1,45 @@
+﻿using JustDecompile.API.Core;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace JustDecompile.Plugins.GoToEntryPoint
+{
+    public class MenuItem : IMenuItem
+    {
+        List<IMenuItem> menuItems = new List<IMenuItem>();
+
+        public MenuItem(string header)
+            : this(header, null)
+        {
+        }
+
+        public MenuItem(string header, ICommand command)
+        {
+            Header = header;
+            Command = command;
+        }
+
+        public ICommand Command
+        {
+            get;
+            set;
+        }
+
+        public object Header
+        {
+            get;
+            set;
+        }
+
+        public object Icon
+        {
+            get;
+            set;
+        }
+
+        public IList<IMenuItem> MenuItems
+        {
+            get { return menuItems; }
+        }
+    }
+}
