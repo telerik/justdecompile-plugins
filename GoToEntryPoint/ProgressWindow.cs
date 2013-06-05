4ea055e8cb45c0fd11f4ee38796a80e431736871
TELERIK\manev <TELERIK\manev>
2013-06-05 15:22:08 +0300

    merge

---
+++ GoToEntryPoint/ProgressWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JustDecompile.Plugins.GoToEntryPoint
+{
+    public class ProgressWindow : Window
+    {
+        public ProgressBar ProgressBar;
+
+        public ProgressWindow()
+        {
+            this.Title = "Searching...";
+
+            this.ResizeMode = System.Windows.ResizeMode.NoResize;
+
+            Grid grid = new Grid { Width = 250, Margin = new Thickness(15) };
+
+            this.ProgressBar = new ProgressBar { HorizontalAlignment = HorizontalAlignment.Stretch, Height = 20 };
+
+            this.ProgressBar.IsIndeterminate = true;
+
+            grid.Children.Add(ProgressBar);
+
+            this.Content = grid;
+
+            this.WindowStyle = WindowStyle.ToolWindow;
+
+            this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterOwner;
+
+            this.Owner = Application.Current.MainWindow;
+
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+        }
+    }
+}
