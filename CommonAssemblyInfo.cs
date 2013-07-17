df64355eebf15decff78a9c3e550bdd5dee08d06
TELERIK\TeamFoundationUser <TELERIK\TeamFoundationUser>
2013-07-17 18:12:46 +0300

    Auto-Build: Version Update [***NO_CI***]

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2013.1.717.0")]
-[assembly: AssemblyFileVersion("2013.1.717.0")]
+[assembly: AssemblyVersion("2013.1.717.1")]
+[assembly: AssemblyFileVersion("2013.1.717.1")]
