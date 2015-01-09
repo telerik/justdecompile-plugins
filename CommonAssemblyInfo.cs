0765360864323e323df918f3041b79ccf2cd95f7
TELERIK\tyovchev <TELERIK\tyovchev>
2015-01-09 19:29:45 +0200

    Updating version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2014.3.1211.0")]
-[assembly: AssemblyFileVersion("2014.3.1211.0")]
+[assembly: AssemblyVersion("2015.1.0109.0")]
+[assembly: AssemblyFileVersion("2015.1.0109.0")]
