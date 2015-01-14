2a2ddaede6c3d9cb5b5c24aa4522173597068e80
TELERIK\JustDecompBuildUser <TELERIK\JustDecompBuildUser>
2015-01-14 17:23:58 +0200

    Auto-Build: Version Update [***NO_CI***]

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2015.1.0109.0")]
-[assembly: AssemblyFileVersion("2015.1.0109.0")]
+[assembly: AssemblyVersion("2015.1.114.0")]
+[assembly: AssemblyFileVersion("2015.1.114.0")]
